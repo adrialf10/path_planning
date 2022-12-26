f0b51106da199848804fe91822f68ccfe23cada0
Adrian <adrian2000alf@gmail.com>
2022-12-26 19:39:17 +0100

    get neigh added

--- test.py
+++ test.py
@@ -3,15 +3,43 @@ from PIL import Image
 
 black = (0, 0, 0)
 white = (255, 255, 255)
+gray = (149, 149, 149)
 red = (255, 0, 0)
+blue = (0, 255, 255)
 
-gm = Image.open("GridMap.png")
-#gm = Image.open("Maze00.png")
 
-gm = gm.convert('RGB')
+def get_neighbours(grid, xy):
+	x = xy[0]
+	y = xy[1]
+	width = grid.width
+	height = grid.height
+	to_return = []
 
-print(gm.getpixel((320,196)))
-gm.putpixel((96,117), red)
+	for i in [-1, 0, 1]:
+		for j in [-1, 0, 1]:
+			if i == 0 and j == 0:
+				continue
+			if x + i < 0 or x + i >= width:
+				continue
+			if y + j < 0 or y + j >= height:
+				continue
+			new_xy = (x + i, y + j)
+			to_return.append((new_xy, grid.getpixel(new_xy)))
+	return to_return
 
-plt.imshow(gm)
+def print_neighbours(grid, n):
+	for point in n:
+		grid.putpixel(point[0], blue)
+
+
+grid = Image.open("Maze00.png")
+grid = grid.convert('RGB')
+
+point = (10, 10)
+
+grid.putpixel(point, red)
+n = get_neighbours(grid, point)
+print_neighbours(grid, n)
+
+plt.imshow(grid)
 plt.show()
